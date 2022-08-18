8e6c0a3e584f582896335e96494f23e8ca7ff4ab
Abel Bokure <abel.ngt@gmail.com>
2022-08-17 22:42:39 -0400

    commit tree 74d94c1a17d9c130a63d8087f57b954e3d1f165d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code here
+const shows = [
+    {
+      id: 1,
+      title: "The Walking Dead"
+    },
+    {
+      id: 2,
+      title: "Loki"
+    },
+    {
+      id: 3,
+      title: "Riverdale"
+    },
+    {
+      id: 4,
+      title: "Game Of Thrones"
+    },
+    {
+      id: 5,
+      title: "Legacies"
+    }
+  ]
+const showContainer = document.getElementById("list")
+
+  function rendershow(shows) {
+   
+    const showName = document.createElement("li")
+    showName.textContent = shows.title 
+    showContainer.append(showName)
+
+  } 
+ function init() {
+    shows.forEach(rendershow)
+ } 
+
+ init()
